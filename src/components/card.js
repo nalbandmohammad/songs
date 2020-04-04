0257c28d4e4b230eacd6924ccd90130c64bd906e
nalbandmohammad <nmohammadrafi11@gmail.com>
2020-04-04 12:08:19 +0530

    Add Welcome Text

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ export default class Cards extends Component {
                 {this.props.userdata.map(user => {
                    return ( 
                     <div className="m-3 p-3 card">                         
-                   <h1>{user.name}</h1>
+                   <h1>Welcome, {user.name}</h1>
                    <p>{user.email}</p>
                    </div>
                    )
